42ed6f31e8d9bcfb2f3438be75ad4dc9fa1960bc
Monisha Periyasamy <monishaperiyasamy777official@gmail.com>
2023-10-05 14:57:48 +0530

    Add files via upload

---
+++ backend Day1/schedule/src/main/java/com/CtrlFreaks/repository/Repository.java
@@ -0,0 +1,11 @@
+package com.CtrlFreaks.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.CtrlFreaks.model.Model;
+
+
+
+public interface Repository extends JpaRepository<Model, Integer>{
+
+}
